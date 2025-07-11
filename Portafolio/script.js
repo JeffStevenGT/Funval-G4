bd3a178defbe0a00b7211e4fbb39af4ff40a0380
JeffGT <jeff.giltoribio@gmail.com>
2025-07-10 21:56:19 -0500

    adicionando

---
+++ Portafolio/script.js
@@ -0,0 +1,11 @@
+new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 250,
+    grabCursor: true,
+    loop: true,
+    breakpoints: {
+        991: {
+        slidesPerView: 3
+        }
+    }
+});
